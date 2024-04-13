3f623faaa5ed7b85fcc7ab15dcf6dde333a9ca91
mohitsinghrajput8349 <119424794+mohitsinghrajput8349@users.noreply.github.com>
2024-04-13 16:32:39 +0530

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -1,1 +1,23 @@
-public class 
+
+//given an integer  find out  the sum of  its digits  using recursion 
+//Inputs n =1234;
+//output 10; 
+//explanation = 1+2+3+4;
+
+public class Main {
+    public static int sumOfDigits(int n) {
+        // base case: if the number is a single digit, return the number itself;
+        if (n < 10) {
+            return n; //return the number
+        }
+        // Recursive case: Add the last digit to the sum of the digits of the remaining number
+        else {
+            return n % 10 + sumOfDigits(n / 10); //if the number is even 
+        }
+    }
+
+    public static void main(String[] args) {
+        int n ;
+        System.out.println("Sum of digits of " + n + " is " + sumOfDigits(n));
+    }
+}
